5de6fd5614d2955cf84f715626aa82a490453c8b
degoeden <degoeden8@gmail.com>
2025-07-28 15:40:50 -0400

    convergence shown, max number is no longer 20, idk what the new is though

--- examples/convergence_script.m
+++ examples/convergence_script.m
@@ -8,23 +8,23 @@ D_feed =  2.125; area_feed = 1071000; D_perm = 25;
 % Feedflow Conditions
 P_feed = 7; x_feed = 50;
 
-
-max_units =  20;
-mdot_W_perm = zeros(max_units, 1);
-x_perm = zeros(max_units, 1);
-for N_units = 1:max_units
+start_units = 21;
+max_units =  50;
+mdot_W_perm = zeros(max_units-start_units+1, 1);
+x_perm = zeros(max_units-start_units+1, 1);
+for N_units = start_units:max_units
     disp(N_units)
     out = sim('examples/membrane_convergence.slx');
-    mdot_W_perm(N_units) = out.simout.mdot_W_perm.Data(end);
-    x_perm(N_units) = out.simout.x_perm.Data(end);
+    mdot_W_perm(N_units-start_units+1) = out.simout.mdot_W_perm.Data(end);
+    x_perm(N_units-start_units+1) = out.simout.x_perm.Data(end);
 end
 
 figure(1)
-line(1:max_units, x_perm, linewidth=2)
+line(start_units:max_units, x_perm, linewidth=2)
 xlabel('Number of Units')
 ylabel('Concentration of Permeate [kg/m^3]')
 
 figure(2)
-line(1:max_units, mdot_W_perm, linewidth=2)
+line(start_units:max_units, mdot_W_perm, linewidth=2)
 xlabel('Number of Units')
 ylabel('Mass flow of Permeate Solvent [kg/s]')
