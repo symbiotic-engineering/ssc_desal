a7653823317e125d1ffb31112221ff3052461dc8
degoeden <degoeden8@gmail.com>
2025-07-31 13:34:50 -0400

    Revert "Revert "shorter membrane convergence""
    This reverts commit 75f194bb13793cffdde1067c8d57d454099c4703.

--- examples/convergence_script.m
+++ examples/convergence_script.m
@@ -2,7 +2,7 @@ clear;clc;close all;
 
 % Membrane Parameters
 A_w = 2.57e-12; B_s = 2.30e-8;
-A_m = 6300; length = 10;
+A_m = 35; length = 1;
 h_ch = 0.7112; eps_sp = 0.85;
 W = A_m/length;
 D_feed =  4*eps_sp / (2/h_ch + (1-eps_sp)*8/h_ch);
@@ -10,10 +10,10 @@ area_feed = W*1e3*h_ch*eps_sp;
 D_perm = 29;
 
 % Feedflow Conditions
-P_feed = 7; x_feed = 50;
+P_feed = 5; x_feed = 32;
 
-start_units = 21;
-max_units =  50;
+start_units = 1;
+max_units =  20;
 mdot_W_perm = zeros(max_units-start_units+1, 1);
 x_perm = zeros(max_units-start_units+1, 1);
 for N_units = start_units:max_units
