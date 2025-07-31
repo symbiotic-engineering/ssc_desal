6945eec70b87341f816b886d84cda5f56b3cfc9d
degoeden <degoeden8@gmail.com>
2025-07-31 10:39:04 -0400

    convergence done and better validation

--- examples/convergence_script.m
+++ examples/convergence_script.m
@@ -3,7 +3,11 @@ clear;clc;close all;
 % Membrane Parameters
 A_w = 2.57e-12; B_s = 2.30e-8;
 A_m = 6300; length = 10;
-D_feed =  2.125; area_feed = 1071000; D_perm = 25;
+h_ch = 0.7112; eps_sp = 0.85;
+W = A_m/length;
+D_feed =  4*eps_sp / (2/h_ch + (1-eps_sp)*8/h_ch);
+area_feed = W*1e3*h_ch*eps_sp;
+D_perm = 29;
 
 % Feedflow Conditions
 P_feed = 7; x_feed = 50;
