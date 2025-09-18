e038b4600067a3724d397f85d5c2cfd68a60d2c4
Erin McGarrity <emcgarri@mathworks.com>
2025-09-18 16:39:50 -0400

    Update calc add both manometers

--- OsmoticPressure.m
+++ OsmoticPressure.m
@@ -13,7 +13,7 @@
 %[text] ## Manometer with Semipermeable Membrane
 %[text] 
 A = 1/100^2;          % [m^2] Cross sectional area
-h0 = 1;               % [m] Initial column height
+h0 = 0.5;               % [m] Initial column height
 ii = 2;               % [1] Charge for NaCl -- there are 2 ions
 c = 0.05;             % [kg/m^3] Salt mass concentration in solution
 m0 = c*A*h0;          % [kg] Salt mass in salty column
@@ -24,23 +24,28 @@ T = 293.15;           % [K] Temperature
 
 % Osmotic Pressure via van 't Hoff:
 pi_os = ii*M*R*T        % [Pa]  %[output:2fce8b33]
-
+% Fluid properties
 rho = 998.2;         % [kg/m^3] Water density (approximate)
 g = 9.81;            % [m/s^2] Gravitational acceleration
-h = pi_os/rho/g %[output:29504609]
+
 % Approximate column height difference 
 % Assumes height change is even on both sides (it isn't)
 fg = @(x)rho*g*x;
 fpo = @(x)ii*m0/M_NaCl/A/(x/2+h0)*R*T;
 fnc = @(x)ii*m0/M_NaCl/A/(x/2+h0)*R*T - rho*g*x;
-xf = fzero(fnc, 0.0) %[output:3e56ad33]
-
-
+xf = fzero(fnc, 0.0) %[output:29504609]
 open_system("simple_manometer");
-sim("simple_manometer");
-h_salt = out.simlog.Salt.h.series.values("m");
-h_pure = out.simlog.Pure.h.series.values("m");
-h_sim = h_salt(end)-h_pure(end) %[output:023bc14d]
+out1 = sim("simple_manometer");
+h_s1 = out1.simlog.Salt.h.series.values("m");
+h_p1 = out1.simlog.Pure.h.series.values("m");
+h_sim1 = h_s1(end)-h_p1(end) %[output:0e815f6f]
+
+open_system("manometer");
+out2 = sim("manometer");
+h_s2 = out2.simlog.h_salty.x.series.values("m");
+h_p2 = out2.simlog.h_pure.x.series.values("m");
+h_sim = h_s2(end)-h_p2(end) %[output:61850a17]
+
 %[text] ## Appendix 1: Derivation of van 't Hoff's Equation 
 %[text] 
 
@@ -53,11 +58,11 @@ h_sim = h_salt(end)-h_pure(end) %[output:023bc14d]
 %   data: {"dataType":"textualVariable","outputData":{"name":"pi_os","value":"4.1705e+03"}}
 %---
 %[output:29504609]
-%   data: {"dataType":"textualVariable","outputData":{"name":"h","value":"0.4259"}}
+%   data: {"dataType":"textualVariable","outputData":{"name":"xf","value":"0.3221"}}
 %---
-%[output:3e56ad33]
-%   data: {"dataType":"textualVariable","outputData":{"name":"xf","value":"0.3608"}}
+%[output:0e815f6f]
+%   data: {"dataType":"textualVariable","outputData":{"name":"h_sim1","value":"0.3221"}}
 %---
-%[output:023bc14d]
-%   data: {"dataType":"textualVariable","outputData":{"name":"h_sim","value":"0.3608"}}
+%[output:61850a17]
+%   data: {"dataType":"textualVariable","outputData":{"name":"h_sim","value":"0.3212"}}
 %---
