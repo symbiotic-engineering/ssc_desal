734cdc7d57e5f4082bb358fda44e0654b382c389
ndegoede <ndegoede@mathworks.com>
2026-08-28 14:46:29 -0400

    3 solutes ion exchange

--- src/+customization/+ion_exchange/k_spec_options.m
+++ src/+customization/+ion_exchange/k_spec_options.m
@@ -1,13 +1,12 @@
-classdef k_input_options < int32
+classdef k_spec_options < int32
 % Enumeration class for choosing which adsorption constant the user
-% specifies directly in the ion_exchange block. The other constant is
-% derived from the selectivity K = k_x / k_y.
-
-% Copyright 2026 The MathWorks, Inc.
+% specifies directly in the ion_exchange block. The other constants are
+% derived from selectivities anchored to the specified k.
 
 enumeration
     k_x(1)
     k_y(2)
+    k_z(3)
 end
 
 methods (Static, Hidden)
@@ -15,6 +14,7 @@ methods (Static, Hidden)
         map = containers.Map;
         map('k_x') = 'Specify k_x';
         map('k_y') = 'Specify k_y';
+        map('k_z') = 'Specify k_z';
      end
 end
 end
